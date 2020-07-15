83c411d2ea0c23d4a7013383bcb24057c3b38a64
Aryan Kumar <kumarayan86@gmail.com>
2020-07-15 12:23:52 +0530

    initial commit

---
+++ src/test/java/com/aryan/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
